5ccfc027968a881cc46edb2e0b4a652a0a29d6bf
Preeti Ghatnekar <52157348+GhatnekarPreeti@users.noreply.github.com>
2021-12-03 20:11:55 +0530

    Create TryWithResources_Example.java

---
+++ TryWithResources_Example.java
@@ -0,0 +1,32 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+public class TryWithResources_Example {
+   public static void main(String args[]) {
+      //Getting the connection
+      String mysqlUrl = "jdbc:mysql://localhost/mydatabase";
+      System.out.println("Connection established......");
+      //Registering the Driver
+      try(Connection con = DriverManager.getConnection(mysqlUrl, "root", "password");
+      Statement stmt = con.createStatement(); ) {
+         try(ResultSet rs = stmt.executeQuery("select * from MyPlayers");){
+            //Retrieving the data
+            while(rs.next()) {
+               System.out.print(rs.getInt("ID")+", ");
+               System.out.print(rs.getString("First_Name")+", ");
+               System.out.print(rs.getString("Last_Name")+", ");
+               System.out.print(rs.getDate("Date_Of_Birth")+", ");
+               System.out.print(rs.getString("Place_Of_Birth")+", ");
+               System.out.print(rs.getString("Country"));
+               System.out.println();
+            }
+         } catch (SQLException e) {
+            e.printStackTrace();
+         }
+      } catch (SQLException e) {
+            e.printStackTrace();
+      }
+   }
+}
